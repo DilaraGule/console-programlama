ab618c3a5a1ff67a9d0485c5e1374390ab60f674
DilaraGule <80154346+DilaraGule@users.noreply.github.com>
2022-02-12 17:34:23 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsim?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisim?");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + surname);
+        }
+    }
+}
